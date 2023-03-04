bfd8430dbe6f2eeed74dad7d5ae4739498fe4fb0
이진영 <swlsdud1005@naver.com>
2023-03-04 14:09:09 +0900

    디테일페이지 디자인 1차

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,6 +1,6 @@
+import "./css/Detail.css";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
 import Review from "../components/Review";
 import {
   dbService,
@@ -66,42 +66,66 @@ export default function Detail() {
       {loading ? (
         <h1>로딩 중...</h1>
       ) : (
-        <>
-          <Link to="/">메인으로</Link>
-          <div>
-            <h2>{detailMove.title}</h2>
-            <img
-              src={`https://image.tmdb.org/t/p/w1280/` + detailMove.poster_path}
-              alt="없음"
-              width="150px"
-              height="200px"
-            />
-            <p>{detailMove.overview}</p>
+        <section className="detailSection">
+          <div className="detailContainer">
+            <div className="detailBox">
+              <div className="detailBoxLeft">
+                <img
+                  src={
+                    `https://image.tmdb.org/t/p/w1280/` + detailMove.poster_path
+                  }
+                  alt="영화포스터"
+                />
+              </div>
+              <div className="detailBoxRight">
+                <div className="detailTitle">{detailMove.title}</div>
+                <div className="detailGenres">
+                  장르&nbsp;:&nbsp;
+                  {detailMove.genres.map((item, key) => (
+                    <span key={key}>{item.name}&nbsp;</span>
+                  ))}
+                </div>
+                <div className="detailRuntime">
+                  시간&nbsp;:&nbsp;{detailMove.runtime}분
+                </div>
+                <div className="detailReleaseDate">
+                  개봉&nbsp;:&nbsp;{detailMove.release_date}
+                </div>
+                <div className="detailOverview">{detailMove.overview}</div>
+              </div>
+            </div>
+            <div className="reviewBox">
+              <div className="reviewWrite">
+                {userInfo !== null ? (
+                  <form onSubmit={onSubmit} className="reviewForm">
+                    <input
+                      type="text"
+                      value={review}
+                      onChange={onChange}
+                      placeholder="평점 및 영화 관람평을 작성해주세요."
+                      required
+                    />
+                    <input type="submit" value="작성" />
+                  </form>
+                ) : (
+                  <div className="reviewNone">
+                    관람평은 로그인 후 작성 가능합니다.
+                  </div>
+                )}
+              </div>
+              <div className="reviewLists">
+                {reviewContents.map((item) => (
+                  <Review
+                    key={item.id}
+                    reviewObj={item}
+                    userInfo={userInfo}
+                    movieTitle={detailMove.title}
+                  />
+                ))}
+              </div>
+            </div>
           </div>
-          <div>
-            {reviewContents.map((item) => (
-              <Review
-                key={item.id}
-                reviewObj={item}
-                userInfo={userInfo}
-                // isOwner={item.creatorId === userInfo.uid}
-                movieTitle={detailMove.title}
-              />
-            ))}
-          </div>
-          {userInfo !== null ? (
-            <form onSubmit={onSubmit}>
-              <input
-                type="text"
-                value={review}
-                onChange={onChange}
-                maxLength={120}
-                required
-              />
-              <input type="submit" value="입력" />
-            </form>
-          ) : null}
-        </>
+        </section>
       )}
     </>
   );
